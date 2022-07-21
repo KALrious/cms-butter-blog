55ff99b1258ee203e9741d923e333ea5e4152f92
JeanGautierSherpas <jean@sherpas.com>
2022-07-21 19:17:17 +0200

    🐛 change header semantic

--- components/header-section/header-section.tsx
+++ components/header-section/header-section.tsx
@@ -2,23 +2,21 @@ import { useEffect, useState, useRef } from 'react';
 
 export default function HeaderSection({}) {
   return (
-    <>
-      <div className="relative bg-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6">
-          <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
-            <div className="flex justify-start lg:w-0 lg:flex-1">
-              <a href="#">
-                <span className="sr-only">Home</span>
-                <img
-                  className="h-8 w-auto sm:h-8"
-                  src="/images/spider.png"
-                  alt="home"
-                />
-              </a>
-            </div>
+    <header className="relative bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <a href="#">
+              <span className="sr-only">Home</span>
+              <img
+                className="h-8 w-auto sm:h-8"
+                src="/images/spider.png"
+                alt="home"
+              />
+            </a>
           </div>
         </div>
       </div>
-    </>
+    </header>
   );
 }
