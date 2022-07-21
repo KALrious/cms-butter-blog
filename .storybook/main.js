5b6a9541637a3595146af5bb74de072c990e171e
JeanGautierSherpas <jean@sherpas.com>
2022-07-21 16:24:38 +0200

    🎨 first story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: [
     '../components/**/*.stories.mdx',
@@ -6,10 +8,21 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  webpackFinal: async (storybookConfig) => {
+    storybookConfig.resolve.plugins.push(new TsconfigPathsPlugin({}));
+    return storybookConfig;
   },
 };
