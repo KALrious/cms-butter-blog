2d56d4118e98aaf9d7b529115003790673bf8703
KALrious <j.gautier180293@gmail.com>
2022-08-29 16:17:31 +0200

    🎨 add linear gradient to hero

--- components/landing-page-sections/hero.tsx
+++ components/landing-page-sections/hero.tsx
@@ -1,3 +1,4 @@
+import WhiteWave from '@/components/svg/white-wave';
 import { FC } from 'react';
 
 type Props = {
@@ -8,7 +9,8 @@ type Props = {
 
 const Hero: FC<Props> = ({ headline, subheadline, scrollAnchorId }) => (
   <section id={scrollAnchorId}>
-    <div className="mx-auto max-w-3xl px-3 mt-16">
+    <div className="mx-auto max-w-3xl px-3 mt-16 before:absolute before:content-[''] before:w-full before:bg-gradient-to-b before:from-violet-300 before:to-sky-200 before:h-5/6 before:top-0 before:left-0 before:-z-10">
+      <WhiteWave className="fill-white	left-0 w-full bottom-[130px] absolute" />
       <div className="flex flex-col gap-4">
         <h1>{headline}</h1>
         <p>{subheadline}</p>
