15174a9c60e9539bb266a3b488e440b8402f84e9
KALrious <j.gautier180293@gmail.com>
2023-02-04 11:56:29 +0100

    🎨 finalizing axeptio cookie banner

--- lib/axeptio-injector.ts
+++ lib/axeptio-injector.ts
@@ -14,19 +14,15 @@ const COOKIE_VERSION = process.env.NEXT_PUBLIC_AXEPTIO_COOKIE_VERSION;
 
 const AxeptioInjector = () => {
   useEffect(() => {
-      useEffect(() => {
-        const el = document.createElement('script');
-        el.setAttribute('src', 'https://static.axept.io/sdk.js');
-        el.setAttribute('type', 'text/javascript');
-        el.setAttribute('async', 'true');
-        el.setAttribute('data-id', AXEPTIO_ID);
-         el.setAttribute('data-cookies-version', COOKIE_VERSION);
-        if (document.body !== null) {
-          document.body.appendChild(el);
-        }
-      }, []);
-
-      return null;
+    const el = document.createElement('script');
+    el.setAttribute('src', 'https://static.axept.io/sdk.js');
+    el.setAttribute('type', 'text/javascript');
+    el.setAttribute('async', 'true');
+    el.setAttribute('data-id', AXEPTIO_ID);
+    el.setAttribute('data-cookies-version', COOKIE_VERSION);
+    if (document.body !== null) {
+      document.body.appendChild(el);
+    }
   }, []);
 
   return null;
