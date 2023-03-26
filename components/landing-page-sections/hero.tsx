e71932d942da1dcaaee1c6930023da04bcdff262
KALrious <j.gautier180293@gmail.com>
2023-03-26 18:59:17 +0200

    💄 add mobile mode

--- components/landing-page-sections/hero.tsx
+++ components/landing-page-sections/hero.tsx
@@ -12,7 +12,7 @@ const Hero: FC<Props> = ({ headline, subheadline, scrollAnchorId }) => (
     <div className="mx-auto max-w-3xl px-3 pt-16 pb-20 before:absolute before:content-[''] before:w-full before:bg-gradient-to-b before:from-violet-300 before:to-sky-200 before:h-full before:top-0 before:left-0 before:-z-10">
       <WhiteWave className="fill-white	left-0 w-full bottom-0 absolute" />
       <div className="flex flex-col gap-4 overflow-y-hidden">
-        <h1 className="animate-titlefadein">{headline}</h1>
+        <h1 className="animate-titlefadein text-4xl sm:text-6xl">{headline}</h1>
         <p className="animate-titlefadein">{subheadline}</p>
       </div>
     </div>
