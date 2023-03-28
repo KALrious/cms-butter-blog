955e17632c4fda2c0412b57f844037d1dd9e2966
KALrious <j.gautier180293@gmail.com>
2023-03-28 13:22:26 +0200

    🐛 temporary remove animation to fix layout shift

--- components/blog/blog.tsx
+++ components/blog/blog.tsx
@@ -10,10 +10,10 @@ const Blog = ({ posts }) => {
         <div className="flex">
           <div className="flex flex-col items-center gap-6">
             <div className="flex">
-              <h2 className="will-change-transform animate-type whitespace-nowrap overflow-hidden text-xl sm:text-4xl lg:text-6xl lg:leading-[1.2]">
+              <h2 className="whitespace-nowrap overflow-hidden text-xl sm:text-4xl lg:text-6xl lg:leading-[1.2]">
                 The Latest Blog Posts:
               </h2>
-              <span className="box-border inline-block w-0 h-8 ml-1 bg-black md:-mb-4 sm:h-11 lg:h-16 animate-cursor will-change-transform rounded-m after:content-[''] after:rounded-m after:absolute after:top-0 after:left-0 after:w-1 after:h-full after:bg-black after:animate-cursor after:will-change-transform"></span>
+              {/* <span className="box-border inline-block w-0 h-8 ml-1 bg-black md:-mb-4 sm:h-11 lg:h-16 animate-cursor will-change-transform rounded-m after:content-[''] after:rounded-m after:absolute after:top-0 after:left-0 after:w-1 after:h-full after:bg-black after:animate-cursor after:will-change-transform"></span> */}
             </div>
             <div className="flex flex-row justify-center gap-4 flex-wrap">
               {posts.map((post) => (
